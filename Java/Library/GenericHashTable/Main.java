d3168c006f98e39c04f6db4c35c665fb10000ab8
Yan Dobel <yanmd132@gmail.com>
2025-10-15 15:35:37 -0300

    Add files via upload

---
+++ Java/Library/GenericHashTable/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+// ==== Just an example of using the HashTable class ====
+// ==== This main class is just for this example xD ====
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       HashTable<String, Double> table = new HashTable<>(10);
+
+       while (true) {
+           System.out.println("1 - Criar novo dado na tabela");
+           System.out.println("2 - Buscar");
+           System.out.println("3 - Mostrar tudo");
+           System.out.println("4 - Sair do programa");
+           int op = sc.nextInt();
+
+           if (op == 1) {
+               System.out.println("Digite a string");
+               String nome = sc.next();
+               System.out.println("Digite o valor");
+               double valor = sc.nextDouble();
+
+               table.insert(nome, valor);
+           }
+           if (op == 2) {
+
+               System.out.println("Digite o primeiro dado");
+               String nome2 = sc.next();
+               System.out.println(table.search(nome2));
+           }
+           if (op == 3) {
+               table.showAll();
+           }
+           if (op == 4) {
+               System.out.println("Programa fechado");
+               break;
+           }
+
+       }
+       sc.close();
+    }
+}
